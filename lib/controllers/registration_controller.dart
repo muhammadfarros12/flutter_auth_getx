8b7267a886e42a5e1773eec2414694a2fd2405bc
ammaar <muhammadfarroos12@gmail.com>
2023-11-02 08:57:23 +0700

    login, register get token(blm save login session)

---
+++ lib/controllers/registration_controller.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_auth_getx/screens/home.dart';
+import 'package:flutter_auth_getx/utils/api_endpoints.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class RegisterationController extends GetxController {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<void> registerWithEmail() async{
+    try {
+      var headers = {'Content-Type': 'application/json'};
+      var url = Uri.parse(
+        ApiEndPoints.baseUrl + ApiEndPoints.authEndPoints.registerEmail
+      );
+
+      Map body = {
+        'name': nameController.text,
+        'email': emailController.text.trim(),
+        'password': passwordController.text
+      };
+
+      http.Response response = await http.post(url, body: jsonEncode(body), headers: headers);
+
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+        if (json['code'] == 0) {
+          var token = json['data']['Token'];
+          print(token);
+          final SharedPreferences? prefs = await _prefs;
+
+          await prefs?.setString('token', token);
+          nameController.clear();
+          emailController.clear();
+          passwordController.clear();
+          Get.off(const HomeScreen());
+        } else {
+          throw jsonDecode(response.body)['message'] ?? 'Unknown error occured';
+        }
+      } else {
+        throw jsonDecode(response.body)['Message'] ?? 'Unknown error occured';       
+      }
+
+    } catch (e) {
+      Get.back();
+      showDialog(
+        context: Get.context!,
+        builder: (context){
+          return SimpleDialog(
+            title: const Text('Error'),
+            contentPadding: const EdgeInsets.all(20),
+            children: [Text(e.toString())],
+        );
+      });
+    }
+  }
+}
---
+++ lib/screens/auth/auth_screen.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth_getx/screens/auth/widgets/input_field.dart';
+import 'package:flutter_auth_getx/screens/auth/widgets/submit_button.dart';
+import 'package:get/get.dart';
+
+import '../../controllers/login_controller.dart';
+import '../../controllers/registration_controller.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  RegisterationController registerationController =
+      Get.put(RegisterationController());
+
+  LoginController loginController = Get.put(LoginController());
+
+  var isLogin = false.obs;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(36),
+          child: Center(
+            child: Obx(
+              () => Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      child: const Text(
+                        'WELCOME',
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MaterialButton(
+                          color: !isLogin.value ? Colors.white : Colors.amber,
+                          onPressed: () {
+                            isLogin.value = false;
+                          },
+                          child: const Text('Register'),
+                        ),
+                        MaterialButton(
+                          color: isLogin.value ? Colors.white : Colors.amber,
+                          onPressed: () {
+                            isLogin.value = true;
+                          },
+                          child: const Text('Login'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    isLogin.value ? loginWidget() : registerWidget()
+                  ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget registerWidget() {
+    return Column(
+      children: [
+        InputTextFieldWidget(registerationController.nameController, 'name'),
+        const SizedBox(
+          height: 20,
+        ),
+        InputTextFieldWidget(
+            registerationController.emailController, 'email address'),
+        const SizedBox(
+          height: 20,
+        ),
+        InputTextFieldWidget(
+            registerationController.passwordController, 'password'),
+        const SizedBox(
+          height: 20,
+        ),
+        SubmitButton(
+          onPressed: () => registerationController.registerWithEmail(),
+          title: 'Register',
+        )
+      ],
+    );
+  }
+
+  Widget loginWidget() {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        InputTextFieldWidget(loginController.emailController, 'email address'),
+        const SizedBox(
+          height: 20,
+        ),
+        InputTextFieldWidget(loginController.passwordController, 'password'),
+        const SizedBox(
+          height: 20,
+        ),
+        SubmitButton(
+          onPressed: () => loginController.loginWithEmail(),
+          title: 'Login',
+        )
+      ],
+    );
+  }
+}
